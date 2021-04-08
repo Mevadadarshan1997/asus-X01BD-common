17290d5bfcecbff536a0422fe1e650faa226761b
Darshan Mevada <43618707+mevadadarshan19@users.noreply.github.com>
2021-04-08 15:46:31 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_X01BD.mk
+    $(LOCAL_DIR)/octavi_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_X01BD-userdebug
+    octavi_X01BD-userdebug
